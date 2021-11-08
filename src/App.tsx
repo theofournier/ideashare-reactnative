6d4df8fc0ab0e94b19a0346fcec1a9c418a63db2
Theo Fournier <theo.fournier@ubisoft.com>
2021-11-07 19:18:00 -0500

    Update configs

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text } from 'react-native';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Idea Share</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
